7ca981f96d22a55cf37378ab8b1c19cbf45dcbab
AndreasBuc <Andreas.Buchner.Rub@gmail.com>
2020-10-06 15:47:21 +0200

    setup project

--- my-nuxt-project/layouts/error.vue
+++ my-nuxt-project/layouts/error.vue
@@ -1,14 +1,7 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <h1>Some Error occured</h1>
+    {{error}}
   </v-app>
 </template>
 
